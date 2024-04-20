2eaab72f9a5c90ed6811b16ae9f85fb72bca76d7
Jose <josedeodanes99@gmail.com>
2024-04-20 12:00:39 -0600

    Update db

--- index1.php
+++ index1.php
@@ -5,6 +5,6 @@ $dBUsername = "domo";
 $dBPassword = "domo.22@012024";
 $dBName = "domotica";
 
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
+//$conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
 ?>
